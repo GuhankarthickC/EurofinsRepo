21668fdb06bec9c99fd3fb99c86243ac4ccf0532
Admin <guhankarthick.c@gmail.com>
2022-05-30 11:47:38 +0530

    New Object Added

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Addodotnet
     {
         static void Main(string[] args)
         {
+            Class1 b = new Class1();
             Class1 c = new Class1();
             /*c.SelectData();*/
             Console.WriteLine("Enter eid,fname,lname,city,country,phoneno");
