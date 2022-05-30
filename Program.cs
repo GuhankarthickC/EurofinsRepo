a770e5fc913ecc1d32ae6b8b526f9f15945de1fa
unknown <guhankarthick.c@gmail.com>
2022-05-30 11:07:21 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Addoreference;
+using System;
+
+namespace Addodotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            /*c.SelectData();*/
+            Console.WriteLine("Enter eid,fname,lname,city,country,phoneno");
+            int eid = Convert.ToInt32(Console.ReadLine());
+            string efname = Console.ReadLine();
+            string elname = Console.ReadLine();
+            string city = Console.ReadLine();
+            string country = Console.ReadLine();
+            string phone = Console.ReadLine();
+            c.InsertData(eid, efname, elname, city, country, phone);
+        }
+    }
+}
